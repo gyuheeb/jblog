75a19f5c75cfadceae591604dbaff1a701a0b4cf
gyuheeb <gyuhee4529@naver.com>
2023-02-14 19:46:45 +0900

    jblog05 -web.xml(x),static resource handler mapping

---
+++ jblog05/src/main/java/com/douzone/jblog/config/web/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.douzone.jblog.config.web;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import com.douzone.jblog.security.LoginInterceptor;
+import com.douzone.jblog.security.LogoutInterceptor;
+
+
+@Configuration
+public class SecurityConfig implements WebMvcConfigurer{
+
+
+	
+	//interceptor 설정
+	@Bean
+	public HandlerInterceptor loginInterceptor() {
+		return new LoginInterceptor();
+	}
+	
+	@Bean
+	public HandlerInterceptor logoutInterceptor() {
+		return new LogoutInterceptor();
+	}
+	
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry
+			.addInterceptor(loginInterceptor())
+			.addPathPatterns("/user/login");
+		
+		registry
+			.addInterceptor(logoutInterceptor())
+			.addPathPatterns("/index");
+
+	}
+	
+	
+	
+}
