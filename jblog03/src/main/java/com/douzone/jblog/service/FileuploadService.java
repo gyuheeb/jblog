16eec0fd4aab725e304931a4f3cf7f9c0cf02214
gyuheeb <gyuhee4529@naver.com>
2023-02-17 16:53:35 +0900

    jblog03 #finish

--- jblog03/src/main/java/com/douzone/jblog/service/FileuploadService.java
+++ jblog03/src/main/java/com/douzone/jblog/service/FileuploadService.java
@@ -12,8 +12,8 @@ import com.douzone.jblog.exception.FileuploadServiceException;
 
 @Service
 public class FileuploadService {
-	private static String SAVE_PATH = "/jblog-uploads";
-	private static String URL_PATH = "/assets/images";
+	private final static String SAVE_PATH="/jblog-uploads/upload-images";
+	private final static String URL_PATH="/assets/upload-images";
 	
 	public String restore(MultipartFile file) {
 		String url = null;
